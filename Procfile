897af6e3d28ccb414532538d2abf6ee87e1c9342
unknown <rohitmadame2010@gmail.com>
2025-03-04 00:33:42 +0530

    new

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn nagarika_sahayata.wsgi --log-file -
+
+web: python manage.py migrate && gunicorn nagarika_sahayata.wsgi
--- nagarika_sahayata/settings.py
+++ nagarika_sahayata/settings.py
@@ -104,7 +104,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT =  'staticfiles'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
