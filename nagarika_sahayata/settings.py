a8963da8b38ea50413b3b12c9c8b677bf3f60919
unknown <rohitmadame2010@gmail.com>
2025-03-04 12:47:43 +0530

    Fix debug

--- nagarika_sahayata/settings.py
+++ nagarika_sahayata/settings.py
@@ -103,7 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://nagarikasahayata-production.up.railway.app/"
+]
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
